0bf8b02a72d0a38d0f3ef7744719a181690f3f5e
vitormanoels.santana <vitormanoels.santana@gmail.com>
2025-04-22 11:57:52 -0300

    FEAT: um pouco do livro mais vendido do mês

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
   import {ref} from "vue";
-
 </script>
 
 <template>
@@ -39,6 +38,17 @@
       </li>
     </ul>
   </header>
+  <main>
+    <div class="maisvendido">
+      <h1>LIVRO MAIS VENDIDO DO MÊS</h1>
+      <h2>Box Jurassic Park - Edição capa dura</h2>
+      <h3>Este box reúne dois grandes clássicos de Michael Crichton: <br> o emblemático Jurassic Park e sua continuação, O Mundo Perdido.<br>
+        Considerado pai do tecnothriller moderno, Crichton criou um universo <br> em que a genética foi capaz de clonar dinossauros extintos há milênios, <br> colocando-os em contato com seres humanos. Dessa ideia resultaram  <br> duas histórias eletrizantes, narradas com muita ação e suspense em uma <br> atmosfera tão perigosa quanto instigante.
+        Os livros serviram de base para <br> o roteiro dos filmes dirigidos por Steven Spielberg, consagrando Jurassic <br> Park como um dos maiores blockbusters da história do cinema.
+      </h3>
+      <img  src="@/assets/jp.jpg" alt="jurassic">
+    </div>
+  </main>
 </body>
 </template>
 
